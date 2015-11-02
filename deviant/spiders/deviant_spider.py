5af06c29681102e740d088a0a41863fcf9260e79
Romitas <surdeykin.denis@gmail.com>
2015-11-02 11:35:36 +0300

    Fix

--- deviant/spiders/deviant_spider.py
+++ deviant/spiders/deviant_spider.py
@@ -7,27 +7,27 @@ import os
 import scrapy
 from selenium import webdriver
 from deviant.items import DeviantItem
+from deviant.config import *
 
 class DeviantSpider(scrapy.Spider):
     name = "deviant"
 
-    allowed_domains = ["art0fck.deviantart.com"]
-    start_urls = [
-#            "http://viaestelar.deviantart.com/gallery/"
-#            "http://blackassassin999.deviantart.com/gallery/51077384/other-fan-art"
-#            "http://blackassassin999.deviantart.com/gallery/51077371/Deus-EX"
-            "http://art0fck.deviantart.com/gallery/"
-            ]
-    folder = allowed_domains[0].split('.')[0]
-
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.start_urls = URLS
+
+        self.allowed_domains = []
+        for i in self.start_urls:
+            domain = i.replace('http://', '')
+            domain = domain.split('/')[0]
+            self.allowed_domains.append(domain)
+
+        self.folder = self.allowed_domains[0].split('.')[0]
 
     def __exit__(self):
         self.driver.close()
 
     def parse(self, response):
-#        for deviation in response.xpath('//a[@class="thumb"]/@href'):
         for deviation in response.xpath('//a[contains(@class,"thumb")]/@href'):
             url = deviation.extract()
             yield scrapy.Request(url, callback=self.parse_deviation)
@@ -39,32 +39,12 @@ class DeviantSpider(scrapy.Spider):
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
-    def parse_mature_content(self, response):
-        download = response.xpath('//img[@collect_rid]')
-
-        if not download and "Mature Content" in response.body:
-            self.driver.get(response.url)
-            month = self.driver.find_element_by_xpath('//input[@id="month"]')
-            day   = self.driver.find_element_by_xpath('//input[@id="day"]')
-            year  = self.driver.find_element_by_xpath('//input[@id="year"]')
-
-            agree   = self.driver.find_element_by_xpath('//input[@id="agree_tos"]')
-            submit  = self.driver.find_element_by_xpath('//input[contains(@class, "submitbutton")]')
-
-            month.send_keys('10')
-            day.send_keys('21')
-            year.send_keys('1990')
-
-            agree.click()
-            submit.click()
-
-
 
     def parse_deviation(self, response):
         download = response.xpath('//img[@collect_rid]')
 
         if not download and "Mature Content" in response.body:
-            #TODO: Use selenium once and to throw cookies to Scrapy, so that it can continue by itself
+            #TODO: Use selenium once and to throw cookies to Scrapy, so that it can continue all by itself
             print response.url + ": Mature Content detected, gonna try to bypass"
             self.driver.get(response.url)
 
@@ -87,13 +67,6 @@ class DeviantSpider(scrapy.Spider):
 
             sel = scrapy.Selector(text=self.driver.page_source)
             download = sel.xpath('//img[@collect_rid]')
-#            download = download[-1].xpath('@src')[0].extract()
-#            print "Got some download from MC section: " 
-#            print download
-#
-#            filename = download.split('/')[-1]
-#
-#            urllib.urlretrieve(download, filename)
 
 
         if download:
