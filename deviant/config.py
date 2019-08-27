4a43f0b7f0e1d01e99be7291b9a500ca3ef7ad17
Romitas <surdeykin.denis@gmail.com>
2019-08-28 01:27:15 +0300

    added download delay, updated config

--- deviant/config.py
+++ deviant/config.py
@@ -2,7 +2,10 @@
 
 import scrapy
 
+OUTPUT_FOLDER = '[output]'
 URLS = [
-        "https://www.deviantart.com/88grzes/gallery/39299171/fantasy"
+#        "https://www.deviantart.com/88grzes/gallery/39299171/fantasy"
+        "https://www.deviantart.com/romitart/favourites/71304017/special",
+        "https://www.deviantart.com/drcurare/gallery"
         ]
 
